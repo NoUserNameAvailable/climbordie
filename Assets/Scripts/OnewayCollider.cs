c35276ae55dd9f32cded2e87c83a2c78121484fb
Nicolas <ery.cnd@gmail.com>
2015-02-25 21:52:08 +0100

    + One-way collision

---
+++ Assets/Scripts/OnewayCollider.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnewayCollider : MonoBehaviour {
+
+	public bool enable;
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (enable && other.gameObject.tag == "Player") {
+			Players player = other.GetComponent<Players>();
+
+			Physics2D.IgnoreCollision (
+				(Collider2D) GetComponent<Collider2D>(),
+				other,
+				player.oneWayCollision
+			);
+		}
+	}
+	
+	void OnTriggerExit2D(Collider2D other) {
+		if (enable && other.gameObject.tag == "Player") {
+			Physics2D.IgnoreCollision (
+				(Collider2D) GetComponent<Collider2D>(),
+				other,
+				false
+			);
+		}
+	}
+
+}
