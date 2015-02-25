6a82e58e66db2bcf0bb04bb9ffceab168f9c9ba9
Nicolas <ery.cnd@gmail.com>
2015-02-25 21:50:36 +0100

    + GameController (In progress)

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+
+}
