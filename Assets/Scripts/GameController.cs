8240b327bf833b6f1ace9485f82caaaadac5a902
Nicolas <ery.cnd@gmail.com>
2015-02-26 17:09:53 +0100

    + GameController script

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,12 +3,44 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+	public GameObject playerPrefab;
+
 	private GameObject player;
+	private GameObject timer;
+	private bool gameRunning = false;
 
 	// Use this for initialization
-	void Start () {
-		player = GameObject.FindGameObjectWithTag ("Player");
+	void Awake () {
+		GameObject spawn = GameObject.FindGameObjectWithTag ("PlayerSpawn");
+
+		// Spawn player
+		player = Instantiate (playerPrefab, spawn.transform.position, Quaternion.identity) as GameObject;
+		player.SetActive (false);
+		spawn.SetActive (false);
+
+		// Start timer
+		timer = GameObject.Find("Timer");
 	}
-	
+
+	void Update() {
+		if (! gameRunning && Input.GetKey (KeyCode.Return))
+			StartGame ();
+	}
+
+	void StartGame() {
+		// Hide start text
+		GameObject startText = GameObject.Find ("StartText");
+		if (startText != null)
+			startText.SetActive (false);
+
+		// Active player
+		player.SetActive (true);
+
+		// Active and start timer
+		if (timer != null) {
+			Timer timerObj = timer.GetComponent<Timer>();
+			timerObj.stop = false;
+		}
+	}
 
 }
