718c438a8b6838df269c0b21888ee0d65cf8f722
Nicolas <ery.cnd@gmail.com>
2015-02-26 17:13:00 +0100

    + Timer script

---
+++ Assets/Timer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+	
+	public int timeLimit;
+	public bool stop;
+
+	private Text textComponent;
+	private bool descending = false;
+	private float counter;
+
+	// Use this for initialization
+	void Start () {
+		textComponent = (Text)GetComponent<Text> ();
+		descending = (timeLimit > 0);
+
+		counter = (descending) ? timeLimit : 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (! stop) {
+			if (descending) {
+				counter -= Time.deltaTime;
+
+				if (counter <= 0) {
+					counter = 0;
+					stop = true;
+				}
+			}
+			else {
+				counter += Time.deltaTime;
+			}
+
+			// Update text
+			textComponent.text = string.Format ("{0:00}:{1:00}:{2:00}", Mathf.Floor (counter / 60), Mathf.Floor (counter % 60), Mathf.Floor ((counter * 100) % 100));
+		}
+	}
+}
