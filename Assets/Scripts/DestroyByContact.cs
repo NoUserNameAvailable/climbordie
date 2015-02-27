e76a8a8a8282d9dc9f80049e1bb6d47fda1a32a3
BoisArnaud <arnaudbois94@gmail.com>
2015-02-27 12:27:31 +0100

    Lava + Implémentation Nicos
    Ajout de la lave avec animation + le code de nicolas sur le départ et la
    taille du bonhomme

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "Player") {
+			Destroy(other.gameObject);
+
+		
+
+		}
+	}
+}
