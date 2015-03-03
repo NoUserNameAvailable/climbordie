6dbc804673b6bf2dc1d488732523d175a73649e2
BoisArnaud <arnaudbois94@gmail.com>
2015-03-03 18:53:50 +0100

    Update IcePlatform
    Correction de colision icePLatform
    Ajout d'une variable de distance

--- Assets/Scripts/IcePlatform.cs
+++ Assets/Scripts/IcePlatform.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class IcePlatform : MonoBehaviour {
 
 	private GameObject lava ;
+	public float distance ;
 
 	
 	// Update is called once per frame
@@ -12,8 +13,8 @@ public class IcePlatform : MonoBehaviour {
 			lava = GameObject.FindGameObjectWithTag ("Lava");
 		else {
 			float sizeFactor = Mathf.Floor(transform.localScale.y + 1);
-			print(transform.position.y -lava.transform.position.y);
-			if((transform.position.y - lava.transform.position.y) <  7 ){
+			//print(transform.position.y -lava.transform.position.y);
+			if((transform.position.y - lava.transform.position.y) <  (distance + 3 ) ){
 				//float sy  = transform.localScale.y * 0.99f  ;
 				float sx  = transform.localScale.x -  (0.0003f  * sizeFactor)  ;
 				Vector3 scale = new Vector3 (sx,/*sy*/transform.localScale.y,transform.localScale.z);
