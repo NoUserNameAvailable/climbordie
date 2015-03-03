36f0789300e4638884d5a3aa72d9e1865e7c0e8c
BoisArnaud <arnaudbois94@gmail.com>
2015-03-03 18:49:13 +0100

    Update  Platform
    Ajout d'une fonte de la glace .
    Finalisation des niveaux rock 1 , 2 et 3 .

---
+++ Assets/Scripts/IcePlatform.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IcePlatform : MonoBehaviour {
+
+	private GameObject lava ;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (lava == null) 
+			lava = GameObject.FindGameObjectWithTag ("Lava");
+		else {
+			float sizeFactor = Mathf.Floor(transform.localScale.y + 1);
+			print(transform.position.y -lava.transform.position.y);
+			if((transform.position.y - lava.transform.position.y) <  7 ){
+				//float sy  = transform.localScale.y * 0.99f  ;
+				float sx  = transform.localScale.x -  (0.0003f  * sizeFactor)  ;
+				Vector3 scale = new Vector3 (sx,/*sy*/transform.localScale.y,transform.localScale.z);
+				transform.localScale = scale ;
+			}
+		}
+			
+		}
+
+}
