753beba51e7c658f1160697591589223eb7b4c61
BoisArnaud <arnaudbois94@gmail.com>
2015-03-04 17:05:00 +0100

    #Add PlatformTombante
    Début de création platform tombante

---
+++ Assets/Scripts/platformTombante.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platformTombante : MonoBehaviour {
+
+	private GameObject lava ;
+	public float distance ;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.tag == "Player") {
+			this.rigidbody2D.isKinematic = false ;
+			this.collider2D.enabled = false ;	
+	}
+}
+}
