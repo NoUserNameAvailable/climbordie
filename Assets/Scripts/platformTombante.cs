d5f061e3d416f390209baf7ee06b98b42088ebc4
BoisArnaud <arnaudbois94@gmail.com>
2015-03-04 17:47:16 +0100

    #UpDate Platforme Tombante
    Correction de la platforme Tombante
    ajout d'un delay avant la chute !

--- Assets/Scripts/platformTombante.cs
+++ Assets/Scripts/platformTombante.cs
@@ -4,12 +4,35 @@ using System.Collections;
 public class platformTombante : MonoBehaviour {
 
 	private GameObject lava ;
-	public float distance ;
+	public float time ;
 
-	void OnTriggerEnter2D(Collider2D collider) {
-		if (collider.gameObject.tag == "Player") {
-			this.rigidbody2D.isKinematic = false ;
-			this.collider2D.enabled = false ;	
+	private bool collide ;
+	private float timeOld ;
+	private float timeNow ;
+
+	void start(){
+		collide = false ;
 	}
-}
+
+	void OnCollisionEnter2D(Collision2D collider) {
+		if (collider.gameObject.tag == "Player" && !collide) {
+		//	print("ok on est en colision");
+			timeOld = Time.realtimeSinceStartup ;
+			collide = true ;	
+		}
+	}
+
+	void Update(){
+		if (collide) {
+		//	print("ok on est en colision update");
+			timeNow = Time.realtimeSinceStartup;
+		//	print((timeNow - timeOld));
+			if( (timeNow - timeOld) > time ) {
+				this.rigidbody2D.isKinematic = false ;
+				this.collider2D.enabled = false ;
+				collide = false ;
+			}
+		}
+
+	}
 }
