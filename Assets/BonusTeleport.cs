a905c58e91b53334076c314b8a4d714e3f0a44bd
BoisArnaud <arnaudbois94@gmail.com>
2015-03-10 18:17:29 +0100

    +Igloo
    Ajout d'un objet Igloo
    Ajout d'un sprite Igloo

---
+++ Assets/BonusTeleport.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BonusTeleport : MonoBehaviour {
+
+	public GameObject recepteur; 
+
+	private Players player ;
+	private bool collide ;
+	private bool activate = false ;
+
+
+	void start(){
+	}
+	
+	void OnTriggerEnter2D (Collider2D col){
+		collide = true;
+		if (col.gameObject.tag == "Player") {
+			if(!activate){
+				activate = true ;
+			}
+			player = col.gameObject.GetComponent<Players>();
+			print(transform.rotation.y);
+			if(transform.rotation.y == 1 ){
+				player.transform.position = new Vector3(recepteur.transform.position.x-0.35f,recepteur.transform.position.y,0) ;  
+			}
+			else if(transform.rotation.y == 0 ){
+				player.transform.position = new Vector3(recepteur.transform.position.x+0.35f,recepteur.transform.position.y,0) ;  
+			}
+			
+		}
+	}
+}
