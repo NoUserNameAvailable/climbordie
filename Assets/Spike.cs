d5b10f6403f58441c42ad7d867998908334ac63e
Nicolas <ery.cnd@gmail.com>
2015-03-10 09:30:39 +0100

    + Pics

---
+++ Assets/Spike.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spike : MonoBehaviour {
+
+	public float damage;
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			other.gameObject.GetComponent<Players> ().takeDamage (damage);
+			other.gameObject.rigidbody2D.velocity = new Vector2 (0.0f, 3.0f);
+		}
+	}
+
+}
