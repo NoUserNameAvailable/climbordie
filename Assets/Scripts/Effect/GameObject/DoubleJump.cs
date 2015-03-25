8a0eedc21bc0867cc52e9adda16b09ce832b8592
Nicolas <ery.cnd@gmail.com>
2015-03-25 11:26:52 +0100

    Update 176 (change in desc)
    - Add spacebar to jump
    - Add object support (to add an object use objectspot prefab)
    - Add logo to ui
    - Add small credits to ui
    - Update object speedup, speeddown and doublejump
    - Update one way collider

---
+++ Assets/Scripts/Effect/GameObject/DoubleJump.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoubleJump : GenericObject {
+
+	void Start() { }
+	
+	public override IEnumerator effect() {
+		Players player = GameController.getInstance ().getPlayer ();
+		
+		player.jumpLimit = 2;
+		
+		yield return new WaitForSeconds (10f);
+		
+		player.jumpLimit = 1;
+	}
+
+}
---
+++ Assets/Scripts/Effect/GameObject/SpeedDown.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedDown : GenericObject {
+
+	private float playerSpeed;
+
+	void Start() {
+		if (GameController.getInstance ().getPlayer () != null)
+			playerSpeed = GameController.getInstance ().getPlayer ().speed;
+		else
+			playerSpeed = 2.0f;
+	}
+
+	public override IEnumerator effect() {
+		Players player = GameController.getInstance ().getPlayer ();
+
+		Blink blink = player.gameObject.AddComponent<Blink> ();
+		blink.StartEffect (2f);
+		
+		player.speed = (float) (playerSpeed / 1.5);
+		
+		yield return new WaitForSeconds (2f);
+
+		player.speed = playerSpeed;
+	}
+
+}
